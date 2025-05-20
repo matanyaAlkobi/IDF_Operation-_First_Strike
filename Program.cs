f7998c3a2a7a2994c0ee6469bbba3b006803fb62
matanya alkobi <matanor123456@gmail.com>
2025-05-20 16:07:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Operation__First_Strike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
