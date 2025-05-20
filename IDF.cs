1760a8136c3a46b82afed0bbbb5538c4234109b9
matanya alkobi <matanor123456@gmail.com>
2025-05-20 17:42:26 +0300

    first commit

---
+++ IDF.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Operation__First_Strike
+{
+    internal class IDF
+    {
+        public DateTime DateOfEstablishment { get; set; }
+        public string CurrentCommander { get; set; }
+        public  List<string> ListOgStrikeOption { get; set; }
+
+        public IDF(DateTime DateOfEstablishment, string CurrentCommander)
+        {
+            this.DateOfEstablishment = DateOfEstablishment;
+            this.CurrentCommander = CurrentCommander;
+            this.ListOgStrikeOption = new List<string>();
+        }
+
+
+
+
+    }
+}
