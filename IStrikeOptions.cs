af559e6640c4fe92989b266db4d703ff420a0e2a
matanyaAlkobi <198562436+matanyaAlkobi@users.noreply.github.com>
2025-05-21 10:08:30 +0300

    Update IStrikeOptions.cs

--- IStrikeOptions.cs
+++ IStrikeOptions.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace IDF_Operation__First_Strike
-{
-    internal interface IStrikeOptions
-    {
-        string name { get; set; }
-
-        
-
-    }
-}
